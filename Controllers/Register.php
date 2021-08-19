6a75e5b9f599e60f3f5a276e47577a88cdc5254c
jep-mikecurtis <mcurtis@jepcapital.com>
2021-08-19 09:34:13 -0700

    Model Update For Users

--- Controllers/Register.php
+++ Controllers/Register.php
@@ -59,11 +59,15 @@ class Register
     $user = \App\User::find(6);
     if (self::is_blocked_domain($request->email)) {
       $user->assignRole($request->role_name ?? 'Unapproved User');
-      \RapydEvents::send_mail('user_registered_blocked', [ 'agent' => $user ]);
+      \RapydEvents::send_mail('user_registered_blocked', [
+        'event_group_model_id'  => $user->id,
+      ]);
       $redirect     = '/registration-awaiting-approval';
     } else {
       $user->assignRole($request->role_name ?? 'Normal User');
-      \RapydEvents::send_mail('user_registered_success', [ 'agent' => $user ]);
+      \RapydEvents::send_mail('user_registered_success', [
+        'event_group_model_id'  => $user->id,
+      ]);
       $redirect     = '/registration-success';
     }
     \RapydEvents::send_mail($rapyd_event, [
